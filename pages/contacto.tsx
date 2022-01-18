00297d30fb98f3a823f29f4bd34b664183a50412
Manuel Silva <manuel.silvag1@gmail.com>
2022-01-17 20:26:09 -0500

    Página de contacto

---
+++ pages/contacto.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import ContactSection from "@components/contact/ContactSection";
+
+const Contacto: NextPage = () => {
+  return (
+    <main className="main">
+      <ContactSection />
+    </main>
+  );
+};
+
+export default Contacto;
