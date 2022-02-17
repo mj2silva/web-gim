518be2b4e2b5133635e81d51d754375600369631
Manuel Silva <manuel.silvag1@gmail.com>
2022-02-16 19:57:24 -0500

    Datos completos - Servicios

--- components/services/ServicesDropdown.tsx
+++ components/services/ServicesDropdown.tsx
@@ -8,6 +8,31 @@ import ServiceDropdownItem from "@components/services/ServiceDropdownItem";
 const services: Service[] = [
   {
     id: 1,
+    title: "GESTIÓN MUNICIPAL Y SANEAMIENTO FÍSICO LEGAL",
+    description:
+      "Se ocupa de guiar u orientar la detección y satisfacción de las necesidades de locales para su óptimo funcionamiento. El saneamiento  fisico legal de predios es la gestión destinada a la regularización de la documentación de la propiedad  del terreno en primer lugar, y luego de la construcción que se haya edificado sobre el predio (de ser el caso) con el objeto de obtener la titulación  o acreditación idonea.",
+    icon: "/img/icon4.png",
+    detail: [
+      {
+        title: "Ver nuestros servicios",
+        items: [
+          "Cambio de razón social en licencia de funcionamiento y/o licencia de Anuncio de publicidad.",
+          "Certificados  de defensa civil (riesgo bajo. medio. alto y muy alto).",
+          "Licencias de funcionamiento o de cesionarios  de locales.",
+          "Obtenci6n de licencia de funcionamiento a consecuencia de renovación  de certificado de defensa civil (riesgo medio, alto y muy alto).",
+          "Autorizaciones  de anunicios de locales.",
+          "Bajas de licencias de funcionamiento de locales.",
+          "Gestiones de defensa civil por VISES y fiscalizaciones municipales.",
+          "Gestiones de ampliacion de giro de licencia de funcionamiento.",
+          "Licencias de demolición, remodelación, ampliación, edificación  nueva y cartas de acondicionamiento de edificaciones.",
+          "Conformidad de Obra.",
+          "Saneamientos Declaratorias de Fábrica. (segun ley N° 29090 y ley N° 27157).",
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
     title: "CONSTRUCCIÓN RETAIL",
     description:
       "Ejecutamos la construcción, remodelación e implementación de locales comerciales integralmente. Mediante procesos de mejora continua, logramos cubrir las necesidades de nuestros clientes de manera rápida y eficaz a través de los siguientes servicios",
@@ -31,7 +56,7 @@ const services: Service[] = [
     ],
   },
   {
-    id: 2,
+    id: 3,
     title: "MANTENIMIENTO RETAIL",
     description:
       "Gestionamos el Mantenimiento Correctivo y Preventivo a través de un servicio personalizado. Planificamos los mantenimientos para asegurar la continuidad operacional de nuestros clientes, ya sea de manera preventiva o correctiva en todas las especialidades.",
